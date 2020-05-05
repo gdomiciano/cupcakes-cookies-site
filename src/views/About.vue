7f632e8721fbdb65a19063fb9235c64b29a0d333
Geisy Domiciano <geisy.domiciano@telenor.se>
2020-05-05 17:46:19 +0200

    :sparkles: add axios

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,65 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="about">
+    <h1 class="section-title">
+      This is another page
+    </h1>
+
+    <img
+      v-if="imgURL"
+      :src="imgURL"
+      class="picture"
+      alt="cute dog"
+    >
+    <p
+      v-else
+      class="error"
+    >
+      {{ errorMsg }}
+    </p>
+  </section>
 </template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      imgURL: '',
+      errorMsg: '',
+    };
+  },
+  mounted() {
+    this.$axios
+      .get('https://dog.ceo/api/breeds/image/randm')
+      .then((response) => {
+        this.imgURL = response.data.message;
+      })
+      .catch((error) => {
+        console.error(error);
+        this.errorMsg = error.message;
+      });
+  },
+};
+</script>
+
+<style>
+
+  .about{
+    margin-bottom: 20px;
+    padding: 20px;
+    display: grid;
+    grid-gap: 20px;
+  }
+
+  .section-title {
+    color: #b8053b;
+    font-family: Signika;
+    font-weight: 700;
+    font-size: 28px;
+    grid-column: span 3;
+  }
+
+  .error{
+    color: red;
+  }
+</style>
